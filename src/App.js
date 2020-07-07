7f43882da3037322f59d7717eaa7cd3b14f93e98
ETBlue <etblue.tw@gmail.com>
2020-07-07 15:39:25 +0800

    [infra] setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { Menu, Grid } from 'semantic-ui-react'
 
 const App = () => {
   return (
     <div className='App'>
+      <Menu secondary pointing>
+        <Menu.Item name='todo' active='{}' />
+      </Menu>
+      <Switch>
+        <Route path='/'>
+          <div>blah</div>
+        </Route>
+      </Switch>
     </div>
   )
 }
